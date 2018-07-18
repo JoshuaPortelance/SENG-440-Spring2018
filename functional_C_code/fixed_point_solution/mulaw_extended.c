e7806dd6165592e9f95fb54bb7c06b3a68ddd878
Jakob Roberts <jakob.m.roberts@gmail.com>
2018-07-18 22:06:41 +0000

    Finished the functional code, might need a bit of cleaning but it is ready for optimization.

--- functional_C_code/fixed_point_solution/mulaw_extended.c
+++ functional_C_code/fixed_point_solution/mulaw_extended.c
@@ -7,55 +7,39 @@
  */
 unsigned int fppwlog2(unsigned int x) {
     /*
-     *  All constants are / 2^8
-     *  0xXX.XX
-     *  x is also assumed to be 2^8
+     *  All constants are  d<<12
+     *  Therefore the "decimal" is located here: 0xY.YYY
+     *  The input x is also in this form.
      */ 
 
-    if( x < 0x0100) {
+    if( x < 0x01000) {
         printf("ERROR\n");
         exit(0);
         return 0xFFFFFFFF;
     }
-    if( x < 0x0200) {
-        // Min return:  0x0
-        // Max return:  0xFF
-        return( x - 0x100); 
+    if( x < 0x02000) {
+        return( x - 0x1000); 
     }
-    if( x < 0x0400) {
-        // Min return:  0x100
-        // Max return:  0x1FF
-        return( 0x0100 + (((x - 0x0200) * 0x0080) >> 8));
+    if( x < 0x04000) {
+        return( 0x01000 + (((x - 0x02000) * 0x00800) >> 12));
     }
-    if( x < 0x0800) {
-        // Min return:  0x200
-        // Max return:  0x2FF
-        return( 0x0200 + (((x - 0x0400) * 0x0040) >> 8));
-    }
-    if( x < 0x1000) {
-        // Min return:  0x300
-        // Max return:  0x3FF
-        return( 0x0300 + (((x - 0x0800) * 0x0020) >> 8));
-    }
-    if( x < 0x2000) {
-        // Min return:  0x400
-        // Max return:  0x4FF
-        return( 0x0400 + (((x - 0x1000) * 0x0010) >> 8));
-    }
-    if( x < 0x4000) {
-        // Min return:  0x500
-        // Max return:  0x5FF
-        return( 0x0500 + (((x - 0x2000) * 0x0008) >> 8));
-    }
-    if( x < 0x8000) {
-        // Min return:  0x600
-        // Max return:  0x6FF
-        return( 0x0600 + (((x - 0x4000) * 0x0004) >> 8));
+    if( x < 0x08000) {
+        return( 0x02000 + (((x - 0x04000) * 0x00400) >> 12));
     }
     if( x < 0x10000) {
-        // Min return:  0x700
-        // Max return:  0x7FF
-        return( 0x0700 + (((x - 0x8000) * 0x0002) >> 8));
+        return( 0x03000 + (((x - 0x08000) * 0x00200) >> 12));
+    }
+    if( x < 0x20000) {
+        return( 0x04000 + (((x - 0x10000) * 0x00100) >> 12));
+    }
+    if( x < 0x40000) {
+        return( 0x05000 + (((x - 0x20000) * 0x00080) >> 12));
+    }
+    if( x < 0x80000) {
+        return( 0x06000 + (((x - 0x40000) * 0x00040) >> 12));
+    }
+    if( x < 0x100000) {
+        return( 0x07000 + (((x - 0x80000) * 0x00020) >> 12));
     }
     printf("ERROR\n");
     exit(0);
@@ -65,50 +49,53 @@ unsigned int fppwlog2(unsigned int x) {
 
 /*
  * Fixed point Mu-Law encoder.
- * Assuming that 0 <= x <= 1
+ * Assuming that in decimal form 0 <= x <= 1
+ * Actual input is: 0 <= x <= 32,768
+ *                  0x0000 <= x <= 0x8000
+ * All values are 16 bit unsigned integers representing d<<15.
+ *      This means there are 15 bits for the decimal.
  * Need 15 bits to represent the decimal portion.
- * So the range is scaled by 2^15:
- *  0 <= x <= 32,768
- *  0x0000 <= x <= 0x8000
+ *  
+ * This function implements the equation:
+ *      (1.0 / 8.0) * log2(1.0 + (255 * x))
+ *
+ * Output is 8 bits with a scaling of d<<7.
+ *      Y.YYY YYYY
  */
 unsigned int encode(unsigned int x) {
-    // 0 - 8000 (hex)
-    
-    // Scale sample from 16 bits to 32 bits.
-    // 1111 111 . 0000...
-    // x.0000....
-    //unsigned int result = x << 16;  //scales from 2^16 to 2^32
-    
-    // [0000 0000 0000 0000]
-    
-    // Perform encoding.
-    //(1.0 / 8.0) * fpwlog2(1.0 + (255 * x));
-    // 1/8 = 0.125. Can use all 16 bits for this so 0.125 * 2^16 = 0x2000
-    // 255 needs 8 bits before decime so: 255 * 2^8 = 0xFF00
-    // 1.0 needs 1 bit for 1 so 1 * 2^15 = 0x8000. Need to line up with 0xFF.00
-    //      so shift it right by 7 to get 0x100
-    // After the addition there will be an extra carry bit so there will be 17 bits total
-    //      need to truncate 1 bit which is the right shift by 1
-
-    
     unsigned int result = x;
     printf("Encode 1: %x \n", result);
-    //       0d255   d<<15   >> 7 = <<8
-    result = (0xFF * result) >> 7;
+    
+    //       0d255   <<15   >> 3 = <<12
+    result = (0xFF * result) >> 3;
     printf("Encode 2: %x \n", result);
-    //       1      + d<<8
-    result = 0x0100 + result;
+    
+    //       1<<12  + d<<12
+    result = 0x1000 + result;
     printf("Encode 3: %x \n", result);
-    //       <<8              >> 4 = <<4
-    result = fppwlog2(result) >> 4;
+    
+    //       <<12  =  <<12
+    result = fppwlog2(result);
 	printf("Encode 4: %x \n", result);
-    //         <<4   *  (1/8)<<4 >>4 = <<4
-    result = ((result * (0x02)) >> 4);
+	
+    //         <<12   * (1/8)<<12  >>12 = <<12
+    result = ((result * (0x200)) >> 12);
     printf("Encode 5: %x \n", result);
-    // 8 bit output, xxxx . xxxx
+    
+    //        <<12   >>5 = <<7
+    result = result >> 5;
+	printf("Encode 6: %x \n", result);
+
     return result;
 }
 
+/*
+ * Fixed point piecewise 2^x.
+ * Using linear approximations.
+ *
+ * Intervals are more narrow afte x > 1 since the
+ * slope start to change rapidly.
+ */
 unsigned int fppw2exp(unsigned int x) {
     // x < 0d0.25
     if (x < 0x0400) {
@@ -126,29 +113,56 @@ unsigned int fppw2exp(unsigned int x) {
     if (x < 0x1000) {
         return(((0x145A * x) >> 12) + 0x0BA6);
     }
-    // x < 0d1.25
-    if (x < 0x1400) {
-        return(((0x1831 * x) >> 12) + 0x0687);
+    // x < 0d1.1
+    if (x < 0x119A) {
+        return(((0x16F6 * x) >> 12) + 0x090A);
     }
-    // x < 0d1.50
+    // x < 0d1.2
+    if (x < 0x1333) {
+        return(((0x18A0 * x) >> 12) + 0x0736);
+    }
+    // x < 0d1.3
+    if (x < 0x14CD) {
+        return(((0x1A62 * x) >> 12) + 0x0519);
+    }
+    // x < 0d1.4
+    if (x < 0x1666) {
+        return(((0x1C46 * x) >> 12) + 0x02A5);
+    }
+    // x < 0d1.5
     if (x < 0x1800) {
-        return(((0x1CCD * x) >> 12) + 0x020C);
+        return(((0x1E4E * x) >> 12) - 0x0034);
     }
-    // x < 0d1.75
-    if (x < 0x1C00) {
-        return(((0x224E * x) >> 12) - 0x0635);
+    // x < 0d1.6
+    if (x < 0x199A) {
+        return(((0x207B * x) >> 12) - 0x0377);
     }
-    // x < 0d2.00
+    // x < 0d1.7
+    if (x < 0x1B33) {
+        return(((0x22D1 * x) >> 12) - 0x0734);
+    }
+    // x < 0d1.8
+    if (x < 0x1CCD) {
+        return(((0x1DD3 * x) >> 12) - 0x0B64);
+    }
+    // x < 0d1.9
+    if (x < 0x1E66) {
+        return(((0x2800 * x) >> 12) - 0x104A);
+    }
+    // x < 0d2.0
     if (x < 0x2000) {
-        return(((0x28B4 * x) >> 12) - 0x1168);
+        return(((0x2AE1 * x) >> 12) - 0x15C3);
     }
     printf("ERROR\n");
     exit(0);
     return 0xFFFFFFFF;
 }
 
-// Does x^8
-// The problem is there is a lot of error here...
+/*
+ * Fixed point x^8
+ *
+ * This function assumes that value of x is scaled by <<12.
+ */
 unsigned int fpexp8(unsigned int x) {
     unsigned int result = x;
 	int i = 0; 
@@ -157,34 +171,45 @@ unsigned int fpexp8(unsigned int x) {
         result = result >> 12;
         printf("x^%d :: %x\n", i + 2, result);
     }
-    
     return result;
 }
 
 /*
  * Fixied point Mu-Law decoder
- * takes in 8-bit
- * puts out 16-bit
+ * takes in 8-bit with a scaling of d<<7.
+ *      Y.YYY YYYY
  *
- * (1/255)((1+255)^x - 1)
- * (1/255)((256)^x - 1)
- * (1/255)((2^x)^8 - 1)
+ * Implements the equation:
+ *      (1/255)((1+255)^x - 1) =
+ *      (1/255)((256)^x - 1) =
+ *      **(1/255)((2^x)^8 - 1)**
+ *
+ * Output is a 16 bit value with a scaling of d<<12
  */
 unsigned int decode(unsigned int x){
     unsigned int result = x;
     printf("Decode 1: %x \n", result);
-    result = result << 8;       // 12 bits of decimal
+    
+    //        <<7    << 5 = <<12
+    result = result << 5;
     printf("Decode 2: %x \n", result);
+    
+    //        <<12
     result = fppw2exp(result);
     printf("Decode 3: %x \n", result);
+    
+    //        <<12
     result = fpexp8(result);
     printf("Decode 4: %x\n", result);
+    
+    //        <<12  -  1<<12   = <<12
     result = result - 0x01000;
     printf("Decode 5: %x \n", result);
-    //result = result * /*(1/255)*/
-    result = result * 0x10;
-    result = result >> 12;
-    //result = result >> /*8?*/;
+    
+    //        (<<12 * (1/255)<<12) >> 12  = <<12
+    result = (result * 0x10) >> 12;
+    printf("Decode 6: %x \n", result);
+    
     return result;
 }
 
@@ -212,8 +237,9 @@ int main(void) {
     /*
         Sanity Checks:
     */
-    printf("Sanity Checks:\n");
     printf("==============\n");
+    printf("Sanity Checks:\n");
+    printf("==============\n\n");
     // 0.1234567 * 2^15
     sample = 0x0FCD;
     test(sample);
@@ -223,10 +249,11 @@ int main(void) {
     test(sample);
 
     /*
-        Tests::
+        Tests:
     */
-    printf("Tests:\n");
     printf("==============\n");
+    printf("Tests:\n");
+    printf("==============\n\n");
     // min
     // 0.0 * 2^15; 
     sample = 0x0000;
